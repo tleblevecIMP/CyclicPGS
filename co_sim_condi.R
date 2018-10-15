dcdb6049250930eaaa84e435bcf54074888ebc4a
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2018-10-15 16:00:52 +0100

    commented gibbs sampling for latemar

--- co_sim_condi.R
+++ co_sim_condi.R
@@ -1,6 +1,5 @@
 # conditioning for four Gaussian functions
-# the covariance matrix is going to be very big
-# probably not inversible, so that we will probably need to use exponential covariances
+# the covariance matrix is thus large
 
 negrnorm<-function(q,mean,sd){
   a=rnorm(1,mean,sd)
@@ -26,6 +25,7 @@ truncnorm<-function(qmin,qmax,mean,sd){
   return(a)
 }
 
+# covariance matrix between data locations with two variables 
 coco_cov_data<-function(data_loc,data_loc_co,dz,dhx,rv,rhx,b,rho,shift){
   # the matrix is going to be non symmetric but positive definit
   # we are here coding the general case where n can be different from n_co
@@ -379,49 +379,52 @@ dual_krig_line<-function(errorwell,rhx,dhx){
 }
 
 
+# this algorithm is based on Emery paper (2013); algorithm 7
+# adapted for four variables reprensenting four Gaussian random functions
+# although in the case studies only three Gaussian functions are used
+# we develop the algo for four Gaussian functions so that it is more generic
+# for the case studies, the third Gaussian function is chosen null
 gibbs_sampling3d<-function(grid,grid_co,q,q_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift,it){
-  # this algorithm is based on Emery paper (2013); algorithm 7
   
-  data_values<-grid[!is.na(grid)] # getting the values
-  data_values_co<-grid_co[!is.na(grid_co)]
+  data_values<-grid[!is.na(grid)] # getting the data values of depositional facies
+  data_values_co<-grid_co[!is.na(grid_co)] # getting the data values of diagenesis
   data_loc<-which(!is.na(grid),arr.ind=TRUE) # getting the position in the grid
-  data_loc_co<-which(!is.na(grid_co),arr.ind=TRUE)
+  data_loc_co<-which(!is.na(grid_co),arr.ind=TRUE) #position can be different if heterotopic data
   n=length(data_values)
   n_co=length(data_values_co) # we have to be careful, because the number of points for the diagenesis might not be the same!
   
-  # defining the thresholds for each Gaussian function
-  thres1=thres2=matrix(0,2,n)
-  thres1[]=thres2[]=c(-Inf,Inf) # default, which works for the values 0 (well extended)
+  # defining the thresholds for each Gaussian function based on the truncation rules used
   
+  # depositional facies
+  thres1=thres2=matrix(0,2,n)
+  thres1[]=thres2[]=c(-Inf,Inf) # default, which works for the values 0 (well extension), there is no condition
   thres1[,data_values==1]=c(-Inf,q[1])
   thres2[,data_values==1]=c(-Inf,q[2])
-  
   thres1[,data_values==2]=c(-Inf,q[1])
   thres2[,data_values==2]=c(q[2],Inf)
-  
   thres1[,data_values==3]=c(q[1],Inf)
   thres2[,data_values==3]=c(q[3],Inf)
-  
   thres1[,data_values==4]=c(q[1],Inf)
   thres2[,data_values==4]=c(-Inf,q[3])
-  
   thres=cbind(thres1,thres2)
+  
+  # diagenetic facies
   thres3=thres4=matrix(0,2,n_co)
   thres3[]=thres4[]=c(-Inf,Inf) # default
   thres4[,data_values_co==3]=c(q_co[1],Inf)
   thres4[,data_values_co==2 & data_values==2]=c(q_co[2],Inf)
   thres4[,data_values_co==2 & data_values==3]=c(q_co[3],Inf)
-  
   thres4[,data_values_co==1 & data_values==2]=c(-Inf,q_co[2])
   thres4[,data_values_co==1 & data_values==3]=c(-Inf,q_co[3])
   thres4[,data_values_co==1 & data_values==4]=c(-Inf,q_co[1])
-  
   thres=cbind(thres,thres3,thres4)
-  # computation of the co-kriging covariance matrix, with four variables !! 
+  
+  # computation of the covariance matrix, with four variables representing the four Gaussian functions 
   c_data<- coco_cov_data3d(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift)
   
   #initialization the two gaussian functions (uncorrelated to each other)
-  # it is better to use one value for each interval
+  # the convergence is better if we initialize with the same value in same intervals
+  #depositional facies
   za=negrnorm(q[1],0,1)
   zb=negrnorm(q[2],0,1)
   zc=posrnorm(q[2],0,1)
@@ -429,7 +432,7 @@ gibbs_sampling3d<-function(grid,grid_co,q,q_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
   ze=posrnorm(q[3],0,1)
   zf=negrnorm(q[3],0,1)
   zg=rnorm(1)
-  Z<-rep(0,2*n+2*n_co)
+  Z<-rep(0,2*n+2*n_co) # vector representing the four Gaussian random functions
   for ( i in 1:n){
     if (data_values[i]==1){
       Z[i] =  za
@@ -453,6 +456,7 @@ gibbs_sampling3d<-function(grid,grid_co,q,q_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
     }
     # we need to be careful now because only three gaussian functions are used
     # and the fourth is used 
+    # diagenetic facies
     zh=posrnorm(q_co[1],0,1)
     zi=posrnorm(q_co[2],0,1)
     zj=posrnorm(q_co[3],0,1)
@@ -501,10 +505,7 @@ gibbs_sampling3d<-function(grid,grid_co,q,q_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
       condi_sup<-(((thres[2,]-Z)/c_data[i,])+Z[i])
       mini=max(pmin(condi_inf,condi_sup),na.rm=TRUE) #eq(21) we can have NA when the covariance is 0 
       maxi=min(pmax(condi_inf,condi_sup),na.rm=TRUE)  #eq(22)
-      #if (abs(pnorm(mini)-pnorm(maxi))<0.0001){print(c(mini,maxi,mean(Z),var(Z)))}
-      #print(c(mini,maxi,mean(Z),var(Z)))
       V = truncnorm(mini,maxi,0,1)
-      #print(c(mini,maxi))
       
       Z = Z + c_data[i,]*(V-Z[i]) # this algorithm allows to modify all the values of the simulation at every step 
     }
@@ -525,11 +526,15 @@ gibbs_sampling3d<-function(grid,grid_co,q,q_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
   return(abind(Z1,Z2,Z3,Z4,along=3))
 }
 
+# covariance matrix with four variables 
+# Z1 and Z2 are simulated where depositional facies are present
+# Z3 is null, and Z4 is simulated where depositional facies are present
 coco_cov_data3d<-function(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift){
-  # the matrix is going to be non symmetric but positive definit
   # we are here coding the general case where n can be different from n_co
+  # idest there can be different locations and number of data of depositional and diagenetic facies
   n=nrow(data_loc)
   n_co=nrow(data_loc_co)
+  #distance matrix for depositional facies data 
   dist_hx <-dist_hy<- dist_v<- matrix(0,n,n)
   for ( i in 1:(n-1)){
     for (j in (i+1):n ){
@@ -539,6 +544,7 @@ coco_cov_data3d<-function(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
       dist_hy[i,j]=dist_hy[j,i]=abs(data_loc[i,2]-data_loc[j,2])*dhy
     }
   }
+  #distance matrix between depositioanal facies data and diagenesis facies data
   dist_hx_co<-dist_hy_co <- dist_v_co<- matrix(0,n,n_co)
   co_dist_hx <- co_dist_hy <- co_dist_v<- matrix(0,n_co,n)
   for ( i in 1:n){
@@ -549,6 +555,7 @@ coco_cov_data3d<-function(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
       dist_hy_co[i,j]=co_dist_hy[j,i]=abs(data_loc[i,2]-data_loc_co[j,2])*dhy
     }
   }
+  #distance matrix between diagenesis facies data
   co_dist_hx_co <-co_dist_hy_co <- co_dist_v_co<- matrix(0,n_co,n_co)
   for ( i in 1:(n_co-1)){
     for (j in (i+1):n_co ){
@@ -558,6 +565,7 @@ coco_cov_data3d<-function(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
       co_dist_hy_co[i,j]=co_dist_hy_co[j,i]=abs(data_loc_co[i,2]-data_loc_co[j,2])*dhy
     }
   }
+  # coefficients of the shifted linear model of co-regionalization model between the four Gaussian random functions
   a12= rho[1]/model(shift[1],rv[1],b[1])
   a14=rho[2]/model(shift[2],rv[1],b[1])
   a24=rho[3]/model(shift[3],rv[2],b[2])
@@ -569,22 +577,20 @@ coco_cov_data3d<-function(data_loc,data_loc_co,dz,dhx,dhy,rv,rhx,rhy,b,rho,shift
   co_cov11x=model_lat(co_dist_hx,rhx[1])
   co_cov11y=model_lat(co_dist_hy,rhy[1])
   
-  
+  # covariances matrix between the four Gaussian random functions Z1 Z2 Z3 and Z4
+  # according to the shifted linear model of coregionalization
   cov12=a12*model(dist_v+shift[1],rv[1],b[1])*cov11x*cov11y # this one is separable
   cov14=a14*model(dist_v_co+shift[2],rv[1],b[1])*cov11x_co*cov11y_co
-  
   cov21=a12*model(dist_v-shift[1],rv[1],b[1])*cov11x*cov11y
   cov41=a14*model(co_dist_v-shift[2],rv[1],b[1])*co_cov11x*co_cov11y
-  
   cov24 = ( a12*a14*model(dist_v_co-shift[1]+shift[2],rv[1],b[1]) + sqrt(1-a12^2)*a24*model(dist_v_co+shift[3],rv[2],b[2]) )*cov11x_co*cov11y_co
   cov42 = (a12*a14*model(co_dist_v+shift[1]-shift[2],rv[1],b[1])+ sqrt(1-a12^2)*a24*model(co_dist_v-shift[3],rv[2],b[2]) )*co_cov11x*co_cov11y
-  
   cov11=model(dist_v,rv[1],b[1])*cov11x*cov11y
   cov22=(a12^2)*model(dist_v,rv[1],b[1])*cov11x*cov11y+(1-(a12)^2)*model_lat(dist_hx,rhx[2])*model_lat(dist_hy,rhy[2])*model(dist_v,rv[2],b[2])
   cov33=model(co_dist_v_co,rv[3],b[3])*model_lat(co_dist_hx_co,rhx[3])*model_lat(co_dist_hy_co,rhy[3])
   cov44=(a14^2)*model(co_dist_v_co,rv[1],b[1])*model_lat(co_dist_hx_co,rhx[1])*model_lat(co_dist_hy_co,rhy[1])+(a24^2)*model(co_dist_v_co,rv[2],b[2])*model_lat(co_dist_hx_co,rhx[2])*model_lat(co_dist_hy_co,rhy[2])+(1-(a14^2)-(a24^2))*model_lat(co_dist_hx_co,rhx[4])*model_lat(co_dist_hy_co,rhy[4])*model(co_dist_v_co,rv[4],b[4])
   
-  # now the cross correlations that equals 0
+  # now the cross correlations that equals 0 because in this case the third Gaussian function is null
   cov34=cov13=cov23=0*dist_v_co
   cov43=cov32=cov31=0*co_dist_v
   
