2710250ebb413d3244788da650ebfb17216ca43d
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2018-10-15 16:14:37 +0100

    deleted useless stuff

--- cosim_functions.R
+++ cosim_functions.R
@@ -1,34 +1,8 @@
-
+# basic simulation and computation of transiograms
 
 # Covariance function used for the Gaussian random function
 model<-function(h,range,b){return(exp(-abs(h/range)^2)*cos(b*h) )}
 model_lat<-function(h,range){return(exp(-(h/range)^2) )}
-
-# basic function to plot a transiogram
-tr_2d_vert<-function(datai,dataj,size,ncell,dz){
-  cov<-numeric(size)
-  for (l in 1:size){
-    npair = nrow(datai)-l
-    t =0
-    for ( pair in 1:npair){
-      for ( y in 1:ncol(datai)){
-        if(is.na(datai[pair,y])== FALSE & is.na(dataj[pair+l,y]) == FALSE){
-          cov[l] = cov[l]+ datai[pair,y]*dataj[pair+l-1,y]
-          t = t+1
-        }
-      }
-    }
-    cov[l]=cov[l]/t
-  }
-  cov = cov /mean(datai,na.rm = TRUE)# we are measuring a correlation
-  dist = (0:(size-1))/(ncell*dz)
-  plot(dist,cov,type = "p",col="black",pch=21,bg="grey",cex=2,ylim=c(0.,1),ylab = "", xlab = "", axes = FALSE)
-  axis(1,at = seq(0,3,0.5),cex.axis=1.5)
-  axis(2,cex.axis=1.5)
-  #mtext("Distance", side = 1, line = 2.5, cex = 1.)
-  #mtext("Transiogram", side = 2, line = 2.5, cex = 1.,las = 0)
-  return(cov)
-}
 
 embedded3d<-function(datai,dataj){
   # we assume there is no NA 
@@ -46,61 +20,8 @@ embedded3d<-function(datai,dataj){
   return(embed/Tii) 
 }
 
-embedded2d<-function(datai,dataj){
-  
-  embed=0
-  Tii=0
-  n=nrow(datai)-1
-  prop=mean(datai[-(n+1),],na.rm=TRUE) # the last line is not counted
-  count=0
-  for ( z in 1:n){
-    for (x in 1:ncol(dataj)){
-      if(is.na(datai[[z,x]])==FALSE & is.na(datai[[z+1,x]])==FALSE){
-        Tii=Tii+datai[[z,x]]*datai[[z+1,x]]
-        embed=embed+datai[[z,x]]*dataj[[z+1,x]]
-        count=count+1
-      }
-      
-    }
-  }
-  Tii=(1-Tii/(prop*count))
-  embed=embed/(prop*count)
-  return(embed/Tii) 
-}
 
-sim2d<-function(rv,rh,freq,N,dz,nz,dx,nx){
-  phase <- runif(N,0,2*pi)
-  bin <- sample(c(-1,1),N,replace=T)
-  w1 <- rnorm(N, mean = freq , sd = sqrt(2)/rv)
-  w2 <- rnorm(N, mean = 0 , sd = sqrt(2)/rh)
-  sim<-array(0,c(nx,nz))
-  for(i in 1:N){
-    for ( z in 1:nz){
-      for(x in 1:nx){
-        sim[x,z] = sim[x,z] + cos(bin[i]*w1[i]*(z*dz)+w2[i]*x*dx+phase[i])
-      }
-    }
-  }
-  sim = sim *sqrt(2/N)
-  
-  return(sim)
-}
-
-sim_specgauss1D<-function(rv,freq,N,dz,nz){
-  phase <- runif(N,0,2*pi)
-  bin <- sample(c(-1,1),N,replace=T)
-  w1 <- rnorm(N, mean = freq , sd = sqrt(2)/rv)
-  sim<-numeric(nz)
-  for(i in 1:N){
-    for ( z in 1:nz){
-        sim[z] = sim[z] + cos(bin[i]*w1[i]*(z*dz)+phase[i])
-    }
-  }
-  sim = sim *sqrt(2/N)
-  
-  return(sim)
-}
-
+# unconditional simulation of a gaussian function with Gaussian cosine covariance
 sim3d<-function(rv,rhx,rhy,freq,N,dz,nz,dx,nx,dy,ny){
   phase <- runif(N,0,2*pi)
   bin <- sample(c(-1,1),N,replace=T)
